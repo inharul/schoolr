18714aab923cd3167f6588c7d3e5a379dab0145a
Inharul Haque <inharulstylo@gmail.com>
2022-11-06 22:51:58 +0530

    app: initialized

---
+++ src/renderer/App.tsx
@@ -0,0 +1,39 @@
+import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="126" alt="icon" src={icon} />
+      </div>
+      <h1>Schoolr</h1>
+      <p>Electron-based School productivity app for everyone!</p>
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Support
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Hello />} />
+      </Routes>
+    </Router>
+  );
+}
